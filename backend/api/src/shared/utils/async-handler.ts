7f772a51d408cc1bd30dc417c0e6591151c17c4c
Junior Martins <amjr.box@gmail.com>
2025-06-13 23:49:43 -0300

    fix(api): corrige o tipo do handler assíncrono para AsyncRequestHandler

--- backend/api/src/shared/utils/async-handler.ts
+++ backend/api/src/shared/utils/async-handler.ts
@@ -1,8 +1,12 @@
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 
-type AsyncRouteHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>
+type AsyncRequestHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<any>
 
-export const asyncHandler = (fn: AsyncRouteHandler) => {
+export const asyncHandler = (fn: AsyncRequestHandler) => {
   return (req: Request, res: Response, next: NextFunction) => {
     Promise.resolve(fn(req, res, next)).catch(next)
   }
