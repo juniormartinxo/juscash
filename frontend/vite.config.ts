cbe0c4aafc24f6c2bf74c86f43e46df156d244da
Junior Martins <amjr.box@gmail.com>
2025-06-16 12:09:59 -0300

    feat(build): adiciona tailwindcss e alias para caminhos no vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
-    host: '0.0.0.0',
-    port: 3000,
-    strictPort: true,
-    watch: {
-      usePolling: true
-    }
+    port: 3001,
+    host: true
+  },
+  preview: {
+    port: 3001,
+    host: true
   }
 })
