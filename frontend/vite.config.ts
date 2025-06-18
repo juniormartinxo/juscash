97111b8a870d226a2f623840010353a06f9fa0fc
Junior Martins <amjr.box@gmail.com>
2025-06-18 07:31:21 -0300

    chore(frontend): atualiza configuração do vite
    - altera porta padrão para 5173
    - adiciona allowedhosts para juscash.juniormartins.dev
    - padroniza aspas simples no arquivo

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
-    port: 3001,
-    host: true
+    port: 5173,
+    host: true,
+    allowedHosts: ['juscash.juniormartins.dev'],
   },
   preview: {
-    port: 3001,
-    host: true
-  }
+    port: 5173,
+    host: true,
+    allowedHosts: ['juscash.juniormartins.dev'],
+  },
 })
